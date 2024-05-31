16cf4f713aa946a8aeef5a5283c6527ccd016bd2
[SW8] 정영준 <wjddudwns567@gmail.com>
2024-05-31 12:47:34 +0900

    Feat: 로그인 모달창 추가

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import { useState } from "react";
 import { useSession, signOut } from "next-auth/react";
-
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
+import Modal from "./Modal";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data, status } = useSession();
+
+  const handleModalClose = () => setIsModalOpen(false);
 
   return (
     <>
@@ -22,20 +25,22 @@ export default function NavBar() {
         </Link>
         <div className="navbar__list">
           {status === "authenticated" ? (
-            <button type="button">
-              {/* onClick={() => signOut()} */}
+            <button type="button" onClick={() => signOut()}>
               로그아웃
             </button>
           ) : (
-            <Link href="/api/auth/signin" className="navbar__list--item">
+            <button
+              type="button"
+              onClick={() => setIsModalOpen(true)}
+              className="navbar__list--item"
+            >
               로그인
-            </Link>
+            </button>
           )}
           <Link href="/users/login" className="navbar__list--item">
             회원가입
           </Link>
         </div>
-        {/* mobile button */}
         <div
           role="presentation"
           className="navbar__button"
@@ -44,7 +49,6 @@ export default function NavBar() {
           {isOpen ? <AiOutlineClose /> : <BiMenu />}
         </div>
       </div>
-      {/* mobile button */}
       {isOpen && (
         <div className="navbar--mobile">
           <div className="navbar__list--mobile">
@@ -81,17 +85,38 @@ export default function NavBar() {
                 로그아웃
               </button>
             ) : (
-              <Link
-                href="/api/auth/signin"
-                className="navbar__list--item--mobile"
-                onClick={() => setIsOpen(false)}
+              <button
+                type="button"
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setIsOpen(false);
+                }}
+                className="navbar__list--item--mobile text-left"
               >
                 로그인
-              </Link>
+              </button>
             )}
           </div>
         </div>
       )}
+      <Modal isVisible={isModalOpen} onClose={handleModalClose}>
+        <div className="flex flex-col gap-3">
+          <img
+            alt="로고 이미지"
+            width={200}
+            height={40}
+            src="/images/large-logo.svg"
+            className="h-[50px]"
+          />
+          <h2 className="text-center font-semibold">로그인</h2>
+          <p>로그인 폼을 여기에 추가하세요.</p>
+          <Link href="/api/auth/signin">
+            <button type="button" onClick={handleModalClose}>
+              Sign in with provider
+            </button>
+          </Link>
+        </div>
+      </Modal>
     </>
   );
 }
